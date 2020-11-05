6d8e856d7c2f4455f51d290ab1b46fccf669997b
ophirporat <57257360+ophirporat@users.noreply.github.com>
2020-11-05 11:47:12 +0200

    5.11

--- src/Payment.java
+++ src/Payment.java
@@ -5,4 +5,15 @@ public abstract class Payment {
     private Date paid;
     private float total;
     private String details;
+    private Account account;
+    private Order order;
+
+    public Payment(String id, Date paid, float total, String details, Account account,Order order) {
+        this.id = id;
+        this.paid = paid;
+        this.total = total;
+        this.details = details;
+        this.account = account;
+        this.order = order;
+    }
 }
