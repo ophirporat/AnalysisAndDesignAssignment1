9b2a1cf1f10299118b6d8455b5187581ad9fc7b8
ophirporat <57257360+ophirporat@users.noreply.github.com>
2020-11-05 13:03:45 +0200

    5.11

--- src/PremiumAccount.java
+++ src/PremiumAccount.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PremiumAccount extends Account {
 
     private ArrayList<Product> products;
 
-    public PremiumAccount() {
+    public PremiumAccount(String id, String billing_address, boolean is_closed, Date open, Date close, int balance, ArrayList<Order> orders, ShoppingCart shoppingCart, ArrayList<Payment> payments, Customer customer) {
+        super(id, billing_address, is_closed, open, close, balance, orders, shoppingCart, payments, customer);
         products =new ArrayList<>();
     }
+
+    public boolean AddProduct(Product product){return products.add(product);}
+
+    public boolean AddProducts(ArrayList<Product> products){return this.products.addAll(products);}
 
 
 }
