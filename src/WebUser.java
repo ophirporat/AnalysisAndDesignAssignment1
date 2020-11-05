0404a8c0fd01fd12eb171272c2997f536b1ab7dd
ophirporat <57257360+ophirporat@users.noreply.github.com>
2020-11-05 12:50:36 +0200

    5.11

--- src/WebUser.java
+++ src/WebUser.java
@@ -5,22 +5,40 @@ public class WebUser {
     }
     private String login_id;
     private String password;
-    private UserStatus state;
+    private UserState state;
     private Customer customer;
+    private ShoppingCart shoppingCart;
 
-    public WebUser(String login_id, String password, UserStatus state, Customer customer) {
+    public WebUser(String login_id, String password, UserState state, Customer customer,ShoppingCart shoppingCart) {
         this.login_id = login_id;
         this.password = password;
         this.state = state;
         this.customer = customer;
+        this.shoppingCart=shoppingCart;
     }
 
-    public boolean HasCustomer(){
-        return customer!=null;
-    }
     public boolean AddCustomer(Customer customer){
-        if (HasCustomer())return false;
+        if (this.customer==null)return false;
         this.customer=customer;
         return true;
+    }
+
+    private boolean HasShoppingcart(){
+        return shoppingCart!=null;
+    }
+    public boolean AddShopphingCart(ShoppingCart shoppingCart){
+        if (!HasShoppingcart()){
+            this.shoppingCart=shoppingCart;
+            return true;
+        }
+        return false;
+    }
+    public boolean RemoveShoppingCart(){
+        if (HasShoppingcart()){
+            shoppingCart.RemoveWebUser();
+            shoppingCart=null;
+            return true;
+        }
+        return false;
     }
 }
